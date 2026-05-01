87798e2ffa370026179cd16afe3e839165163049
Jozef <81496018+jozmas@users.noreply.github.com>
2026-05-01 17:21:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'waveradio-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
-
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  console.log('SW Installed');
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  // Povinné pre inštaláciu - aspoň prázdny fetch handler
 });
