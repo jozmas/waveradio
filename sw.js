d6bc5c6291822948db5e7f6e58cd6a3e0b6891bc
Jozef <81496018+jozmas@users.noreply.github.com>
2026-05-01 15:56:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
